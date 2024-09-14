54f8025cd1e691ccac70ed6e1752812be0207d5c
emilakerman <emilaakerman@gmail.com>
2024-09-14 14:21:47 +0200

    added get random name endpoint

--- simple_rest/src/main/java/com/example/simple_rest/Controller.java
+++ simple_rest/src/main/java/com/example/simple_rest/Controller.java
@@ -2,12 +2,20 @@ package com.example.simple_rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @RestController
 public class Controller {
+    String[] names = {"Emil", "Dennis", "Birger", "Danne"};
+
     @GetMapping("/api/test")
     public String getString() {
         return "Hello2";
     }
-    
+    @GetMapping("/api/randomName")
+    public String getRandomName() {
+        Random rand = new Random();
+        int name = rand.nextInt(names.length);
+        return names[name];
+    }
 }

e920c264277f1575e1bf07e2c511a8e4092d4ebe
emilakerman <emilaakerman@gmail.com>
2024-09-24 14:09:10 +0200

    added post method with pathvariable

--- simple_rest/src/main/java/com/emilakerman/simple_rest/NameController.java
+++ simple_rest/src/main/java/com/emilakerman/simple_rest/NameController.java
@@ -1,6 +1,7 @@
 package com.emilakerman.simple_rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,13 @@ public class NameController {
         int name = rand.nextInt(names.toArray().length);
         return names.get(name);
     }
-    @PostMapping("/add")
-    public void postName(@RequestBody String name) {
+    @GetMapping("/all")
+    @ResponseBody
+    public ArrayList<String> getAllNames() {
+        return names;
+    }
+    @PostMapping("/add/{name}")
+    public void postName(@PathVariable String name) {
         names.add(name);
     }
 }
